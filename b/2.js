3564bc5d7868a1e77ded0be9472a7b4caa1161c0
cwf <cwf818@126.com>
2019-07-02 16:05:40 +0800

    2 done

--- b/2.js
+++ b/2.js
@@ -3,37 +3,72 @@ var fs = require('fs');
 var querystring = require('querystring');
 var urlLib = require('url');
 
+var users = {};
+
 var server = http.createServer(function(req, res){
-	//console.log(req.url);
 	// 处理GET数据
-	//var arr = req.url.split('?');
-	// arr[0]  /user
-	// arr[1]  user=zhangsan&pass=1234
-	//var GET = querystring.parse(arr[1]);
 	var URL = urlLib.parse(req.url, true).pathname;
-	var GET = urlLib.parse(req.url, true).query;	
-	
+	var GET = urlLib.parse(req.url, true).query;
 	var POST = {};
 	var str = '';
 	// 处理POST数据
 	req.on('data', function(data){
-		console.log('data event');
 		str += data;
 	});
 	req.on('end', function(){
-		console.log('end event');
 		POST = querystring.parse(str);
-
-		console.log(URL, GET, POST);
+		console.log(URL)
 		
-		fs.readFile(URL.substring(1), function(err, data){
-			if (err){
-				console.log("404");
-			} else {			 
-				res.write(data);
-			}		
+		if (URL=='/user') {
+			// 处理user接口相关事务： register / login
+			switch (GET.act) {
+				case 'reg': 
+					if (users[GET.user]) {						
+						res.write('user exist! failed!');
+						res.end();
+					}else {
+						users[GET.user] = GET.pass;									
+						console.log(users);
+						res.write('register successfully!');
+						res.end();
+					}
+					break;
+				case 'login':
+					if (users[GET.user] == null) {						
+						res.write('login failed, user does not exist!');
+						res.end();
+					} else if (users[GET.user] != GET.pass) {
+						res.write('login failed, incorrect user or password!');
+						res.end();
+					} else {						
+						res.write('login successfully!');
+						res.end();
+					}
+					break;
+				default:
+					res.write('unknown action!');
+					res.end();
+			}
+		} else if (URL=='/search') {
+			res.write(`<html>
+				<head>
+					<title>Nodejs</title>
+					<meta http-equiv="refresh" content="1;URL=https://www.baidu.com/s?wd=${GET.keyword}">
+				</head>
+				<body>
+				</body>
+				</html>`);
 			res.end();
-		})		
+		} else {		
+			fs.readFile(URL.substring(1), function(err, data){
+				if (err){
+					console.log("404");
+				} else {			 
+					res.write(data);
+				}		
+				res.end();
+			})		
+		}
 	});	
 });
 server.listen(8888);
