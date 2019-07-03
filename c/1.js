2af4e75a18afe730dc18645ccc185142476ed3b1
cwf <cwf818@126.com>
2019-07-03 16:10:03 +0800

    3 done

---
+++ c/1.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const cookieSession = require('cookie-session')
+const app = express()
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser('jsdksd'))
+app.use(cookieSession({name: 'session',keys: ['key1', 'key2','sfdjkkfjsk', 'dskwje']}))
+app.use(express.static('./www'))
+
+app.get('/cookie', function(req, res){
+	res.cookie('name', 'cwf')
+	res.send('OK')
+})
+
+
+app.get('/countrefresh', function(req, res){
+	console.log(req.session)
+	var n = req.session.views || 0
+	req.session.views = ++n
+	res.send(n + ' views')
+})
+
+app.get('/readsession', function(req, res){
+	console.log(req.session)
+	res.send('OK')
+})
+
+app.get('/session', function(req, res){
+	req.session['user'] = 'cwf'
+	res.send('OK')
+})
+
+app.get('/readcookie', function(req, res){
+	res.send({cookies: req.cookies, signedCookies: req.signedCookies})
+})
+
+app.get('/scookie', function(req, res){		
+	res.cookie('name2', 'cwf',{ signed: true } )
+	res.send('OK')
+})
+
+
+app.get('/user', function (req, res) {
+  console.log(req.query)
+  res.send('OK')
+})
+
+app.get('/search', function(req, res) {
+	res.redirect(`https://www.baidu.com/s?wd=${req.query.keyword}`)
+})
+
+app.post('/user', function (req, res) {
+  console.log(req.body)
+  res.send('OK')
+})
+
+ 
+app.listen(3000)
